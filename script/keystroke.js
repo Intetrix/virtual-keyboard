710786453d107665aa28a7396f1ce1bb40842622
Tanya Sevko <taniasevko@yandex.ru>
2023-04-30 20:13:58 +0300

    feta: add arrow render on textarea

--- script/keystroke.js
+++ script/keystroke.js
@@ -2,10 +2,19 @@ import { arrayOfKeys } from "./keysArray.js";
 
 export class KeyStroke {
   constructor() {
+    const textarea = document.querySelector(".textarea");
     document.addEventListener("keydown", (event) => {
       arrayOfKeys.forEach((el) => {
         if (event.code === el.code) {
           document.getElementById(`${event.code}`).classList.add("active");
+          if (
+            event.code === "ArrowUp" ||
+            event.code === "ArrowLeft" ||
+            event.code === "ArrowRight" ||
+            event.code === "ArrowDown"
+          ) {
+            textarea.value += el.text;
+          }
         }
       });
     });
