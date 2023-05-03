1ede26a46b6424d6c9fdc62c86b14c76af71fa01
Tanya Sevko <taniasevko@yandex.ru>
2023-05-03 13:21:51 +0300

    fix: mouse click on some buttons

--- script/controller.js
+++ script/controller.js
@@ -4,23 +4,24 @@ export class MouseClick {
   constructor() {
     const keys = document.querySelectorAll('.keys');
     const textarea = document.querySelector('.textarea');
-    /*arrayOfKeys.forEach((obj) => {
-      keys.forEach((el) => {
-        if (obj.code === el.id);
-        el.onclick = (event) => {
-            textarea.value += event.target.textContent;
-        }
-      });
-    });*/
     arrayOfKeys.forEach((obj) => {
       keys.forEach((el) => {
         if (el.id === obj.code && (obj.type === 'letter' || obj.type === 'number' || obj.type === 'arrow')) {
           el.addEventListener('click', (event) => {
             textarea.value += obj.text;
           });
-        } else if (el.id === obj.code && (obj.type === 'tab')) {
+        } else if (el.id === obj.code && obj.type === 'tab') {
           el.addEventListener('click', (event) => {
             textarea.value += '    ';
+          });
+        } else if (el.id === obj.code && obj.type === 'enter') {
+          el.addEventListener('click', (event) => {
+            textarea.value += `\n`;
+          });
+        } else if (el.id === obj.code && obj.type === 'backspace') {
+          el.addEventListener('click', (event) => {
+            let str = textarea.value;
+            textarea.value = str.slice(0,- 1);
           });
         }
       });
